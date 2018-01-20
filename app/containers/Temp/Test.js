89fee40679c2beef18d0915bc1d6f00618d9e625
Duy <intelligent2610@gmail.com>
2018-01-20 22:00:32 +0700

    convert css

--- app/containers/Temp/Test.js
+++ app/containers/Temp/Test.js
@@ -1,33 +1,110 @@
 import React from 'react';
 import { connect } from "react-redux";
 import PropTypes from 'prop-types';
-import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Image } from 'react-native';
 import RNC from 'react-native-css';
+import { vw, vh, vmin, vmax, getPercentWidth, getPercentHeight } from '../../utils/scale'
+
+var HOST = 'http://swastika.io'
+//min-height = minHeight
+//max-height = maxHeight
+//min-width = minWidth
+//max-width = maxWidth
+//60vh = 60 * vh
+//440px = 440
+//url('../assets/img/bg32.jpg') = /assets/img/bg32.jpg
+// width: ${getPercentWidth(100)};   %
+// height: ${getPercentHeight(100)}; %
+// z-index = zIndex
+// background-size: cover; = resizeMode: cover;
+// background-position: center center; = ??
+// margin-right = marginRight
+// margin-right = marginRight
+
+const styleBootstrap = StyleSheet.create({
+    'page-headerpage-header-small': {
+        'minHeight': 60 * vh,
+        'maxHeight': 100
+    }
+    ,
+    'page-header': {
+        'minHeight': 100 * vh,
+        'maxHeight': 999,
+        'padding': 0,
+        'backgroundColor': '#FFFFFF',
+        'position': 'relative',
+    },
+    "row": {
+        "display": "flex",
+        "marginRight": -15,
+        'flex': 1,
+        "marginLeft": -15
+    },
+    'page-header-image': {
+        'position': 'absolute',
+        'resizeMode': 'cover',
+        'width': getPercentWidth(100),
+        'height': getPercentHeight(100),
+        'zIndex': -1,
+    },
+    'content-center': {
+        'top': getPercentHeight(10)
+    }
+    , "col_md_8": {
+        "maxWidth": getPercentWidth(66.666667)
+    }
+    , "title": {
+        "fontWeight": "700",
+        "paddingTop": 30
+    }
+})
 
 const stylesRNC = RNC
     `
+    .page-header.page-header-small {
+        minHeight: ${60 * vh};
+        maxHeight: 440;
+    }
+
     .page-header {
-        minHeight: 100;
-        maxHeight: 500;
+        min-height: 100vh;
+        max-height: 999px;
         padding: 0;
-        backgroundColor: #FFFFFF;
+        color: #FFFFFF;
         position: relative;
     }
+
+    .page-header-image {
+        position: absolute;
+        resizeMode: cover;
+        width: ${getPercentWidth(100)};
+        height: ${getPercentHeight(100)};
+        zIndex: -1;
+    }
+    .content-center {
+        top: ${getPercentHeight(57)};
+    }
+
+    .row {
+        display: flex;
+        marginRight: -15;
+        marginLeft: -15;
+      }
     
       `;
 //swastika.io
 export const jsonData = [{
     "componentType": "View",
-    "styleName": "page-header",
+    "styleName": "page-header page-header-small",
     "id": "1",
     "dataType": "Component",
     "dataSource": [
         {
-            "componentType": "Text",
+            "componentType": "View",
             "styleName": "page-header-image",
             "id": "2",
             "dataType": "background",
-            "dataSource": "url('../assets/img/bg32.jpg')"
+            "dataSource": "/assets/img/bg32.jpg"
         }, {
             "componentType": "View",
             "styleName": "content-center",
@@ -61,13 +138,46 @@ export const jsonData = [{
         }]
 }]
 
+function getListStylesByStyleName(listStyleName) {
+    let arrStyle = [];
+    for (let styleName of listStyleName) {
+        if (typeof styleBootstrap[styleName] !== 'undefined')
+            arrStyle.push(styleBootstrap[styleName])
+    }
+    return arrStyle;
+}
+
+//Chuyển đổi style name từ Css ra React Style names
+function convertReactStyleNames(strData) {
+    var newStyleName = [];
+    let splitStyles = strData.split(' ')
+    var index = 0;
+    var fullStyleName = '';
+    for (let styleName of splitStyles) {
+        newStyleName[index] = styleName;
+        fullStyleName = fullStyleName.concat(styleName)
+        index++;
+    }
+    newStyleName[index] = fullStyleName;
+    return newStyleName;
+}
+
 function generateTag(data) {
-    var componentCustome;
     let Arr = (data).map((entry, i) => {
         if (entry.componentType == 'View') {
-            return <CustomView key={i + 'CustomView'} id={entry.id} styleName={entry.styleName} dataSource={entry.dataSource} />
+            if (entry.dataType == 'background') {
+                return <CustomImage key={i + 'CustomImage'}
+                    id={entry.id} styleName={convertReactStyleNames(entry.styleName)}
+                    dataSource={entry.dataSource} dataType={entry.dataType} />
+            } else {
+                return <CustomView key={i + 'CustomView'}
+                    id={entry.id} styleName={convertReactStyleNames(entry.styleName)}
+                    dataSource={entry.dataSource} dataType={entry.dataType} />
+            }
         } else if (entry.componentType == 'Text') {
-            return <CustomText key={i + 'CustomText'} id={entry.id} styleName={entry.styleName} dataSource={entry.dataSource} />
+            return <CustomText key={i + 'CustomText'}
+                id={entry.id} styleName={convertReactStyleNames(entry.styleName)}
+                dataSource={entry.dataSource} dataType={entry.dataType} />
         }
     })
 
@@ -91,7 +201,7 @@ export default connect()(MyScreen)
 
 class CustomText extends React.Component {
     static propTypes = {
-        styleName: PropTypes.string.isRequired,
+        styleName: PropTypes.array.isRequired,
         // styles: PropTypes.oneOfType([
         //     PropTypes.array,
         //     PropTypes.number,
@@ -101,16 +211,16 @@ class CustomText extends React.Component {
 
     render = () => {
         const { styleName, dataSource, id } = this.props;
-        // style={styles[styleName]}
+
         return (
-            <Text key={id} style={stylesRNC[styleName]}>{dataSource}</Text>
+            <Text key={id} style={getListStylesByStyleName(styleName)}>{dataSource}</Text>
         );
     }
 }
 
 class CustomView extends React.Component {
     static propTypes = {
-        styleName: PropTypes.string.isRequired,
+        styleName: PropTypes.array.isRequired,
         // styles: PropTypes.oneOfType([
         //     PropTypes.array,
         //     PropTypes.number,
@@ -118,13 +228,43 @@ class CustomView extends React.Component {
         // ]).isRequired,
     }
 
+    constructor(props) {
+        super(props)
+        const { styleName } = this.props;
+        this.styleC = getListStylesByStyleName(styleName);
+        for (let styleChild of this.styleC) {
+            if (styleChild.hasOwnProperty('color')) {
+                styleChild['backgroundColor'] = styleChild['color']
+                delete styleChild['color']
+            }
+        }
+    }
+
     render = () => {
-        const { styleName, dataSource, id } = this.props;
+        const { styleName, dataSource, id, dataType } = this.props;
         // style={styles[styleName]}
         return (
-            <View key={id} style={stylesRNC[styleName]}>
+            <View key={id} style={this.styleC}>
                 {generateTag(dataSource)}
             </View>
+        );
+    }
+}
+
+class CustomImage extends React.Component {
+    static propTypes = {
+        styleName: PropTypes.array.isRequired,
+    }
+
+    converImageURL(image) {
+        return HOST + image
+    }
+
+    render = () => {
+        const { styleName, dataSource, id, dataType } = this.props;
+        return (
+            <Image key={id} style={getListStylesByStyleName(styleName)}
+                source={{ uri: this.converImageURL(dataSource) }} />
         );
     }
 }
--- app/utils/scale.js
+++ app/utils/scale.js
@@ -11,4 +11,19 @@ const scale = size => width / guidelineBaseWidth * size;
 const scaleVertical = size => height / guidelineBaseHeight * size;
 const scaleModerate = (size, factor = 0.5) => size + (scale(size) - size) * factor;
 
-export { scale, scaleVertical, scaleModerate};
+//Base on "react-native-viewport-units"
+//Ref: https://github.com/jmstout/react-native-viewport-units
+const vw = width / 100
+const vh = height / 100
+const vmin = Math.min(vw, vh);
+const vmax = Math.max(vw, vh);
+
+export const getPercentWidth = (widthPer) => {
+    return widthPer * width / 100
+}
+
+export const getPercentHeight = (heightPer) => {
+    return heightPer * height / 100
+}
+
+export { scale, scaleVertical, scaleModerate, vw, vh, vmin, vmax };
